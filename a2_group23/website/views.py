263cc1875c684cd05c8ce706ddb2e92c9ccb9ff1
KyleQUT <n11248416@qut.edu.au>
2025-10-23 19:27:25 +1000

    Changes to index and views for filtering events
    Made changes to index filter buttons so that they work (still need to test), also added back create event html and made small fix in views to date_time

--- a2_group23/website/views.py
+++ a2_group23/website/views.py
@@ -35,12 +35,12 @@ def search():
         return redirect(url_for('main.index'))
 
 # Backend Filter (working on - Still need to do front end)
-# @main_bp.route('/filter-event/<category>')
-# def filter_event(category):
-    # filtered = db.session.scalars(
-        # db.select(Event).join(Category).where(Category.category_name.ilike(category)).order_by(Event.time.asc())
-        # ).all()
-        # return render_template('index.html', events=filtered)    
+@main_bp.route('/filter-event/<category>')
+def filter_event(category):
+    filtered = db.session.scalars(
+        db.select(Event).join(Category).where(Category.category_name.ilike(category)).order_by(Event.date_time.asc())
+        ).all()
+    return render_template('index.html', events=filtered)    
     
 @main_bp.route('/events/<int:event_id>')
 def event_detail(event_id):
