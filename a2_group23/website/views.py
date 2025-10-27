de88bf2bc733bc40b594c8ef65183a7920be0ffb
FeliciaOye <n12467065@qut.edu.au>
2025-10-27 17:20:45 +1000

    error handl

--- a2_group23/website/views.py
+++ a2_group23/website/views.py
@@ -241,4 +241,15 @@ def venues():
 
 
 
+#error handling
+@main_bp.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@main_bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template ('500.html'), 500
+
+
 
