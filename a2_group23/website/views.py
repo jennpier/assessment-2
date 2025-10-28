b293a48d450f8c67b44532e19b5342e3102f7891
KyleQUT <n11248416@qut.edu.au>
2025-10-28 19:33:20 +1000

    Fixed Search Bar in Views
    Fixed the search bar so can search for both event title and description

--- a2_group23/website/views.py
+++ a2_group23/website/views.py
@@ -28,7 +28,7 @@ def search():
     if request.args['search'] and request.args['search'] != "":
         print(request.args['search'])
         query = "%" + request.args['search'] + "%"
-        events = db.session.scalars(db.select(Event).where(Event.description.like(query))).all()
+        events = db.session.scalars(db.select(Event).where((Event.title.like(query)) | (Event.description.like(query)))).all()
         return render_template('index.html', events=events)
     else:
         return redirect(url_for('main.index'))
